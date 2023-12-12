915ca410ff305786f6f592100dc1b9d92b73e6d6
VILHALVA <107000099+VILHALVA@users.noreply.github.com>
2023-12-12 17:14:57 -0500

    CURSO COMPRETO

---
+++ CURSO COMPRETO/07) APPS - FUNDAMENTOS/Mastering the Editor/PSSourceControlSolution/PSClassLibrary/CalculatorMethods.cs
@@ -0,0 +1,37 @@
+﻿namespace PSClassLibrary
+{
+	public static class CalculatorMethods
+	{
+		public static float Add(float x, float y)
+		{
+			Console.WriteLine("Adding {0} plus {1}", x, y);
+			return x + y;
+		}
+
+		public static float Subtract(float x, float y)
+		{
+			Console.WriteLine("Subtracting {0} minus {1}", x, y);
+			return x - y;
+		}
+
+		public static float Multiply(float x, float y)
+		{
+			Console.WriteLine("Multiplying {0} times {1}", x, y);
+			return x * y;
+		}
+
+		public static float Divide(float x, float y)
+		{
+			Console.WriteLine("Dividing {0} divided by {1}", x, y);
+			if (y == 0)
+			{
+				return 0;
+			}
+			else
+			{
+				return x / y;
+			}
+		}
+
+	}
+}
